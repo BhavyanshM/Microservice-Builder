08d6b85bb1a18724b48b760d3d744dcab804399c
BhavyanshM <bhavyanshmishra@gmail.com>
2017-08-21 16:55:13 -0500

    Add a button and make it work

--- src/application/MainApplication.java
+++ src/application/MainApplication.java
@@ -1,10 +1,29 @@
 package application;
 
-public class MainApplication {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class MainApplication extends Application{
+
+	private Stage window;
+	private Parent root;
+	private Scene scene;
+	private FXMLLoader loader;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		launch(args);
 	}
 
+	public void start(Stage primaryStage) throws Exception{
+		window = primaryStage;
+		loader = new FXMLLoader();
+		root = loader.load(getClass().getResource("/views/Main.fxml").openStream());
+		scene = new Scene(root, 800, 600);		
+		window.setScene(scene);
+		window.show();
+	}
 }
